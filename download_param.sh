dd2d8dffc0ba1ac0e629c36946a5c66e2e6131a2
taserbeat <taserbeat@gmail.com>
2022-09-05 22:32:22 +0900

    chore: パラメータファイルのダウンロードスクリプトを作成

---
+++ download_param.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+<<COMMENTOUT
+Download heavy size parameter files.
+COMMENTOUT
+
+# -------------------------------------------------------------------------------------------
+
+readonly SCRIPT_EXECUTED_DIR=$(pwd)
+readonly SCRIPT_DIR=$(
+  cd $(dirname $0)
+  pwd
+)
+
+readonly TARGETS=("all" "ch06")
+
+# -------------------------------------------------------------------------------------------
+
+function download_ch06() {
+  cd "${SCRIPT_DIR}/ch06"
+
+  curl -O https://github.com/taserbeat/zero-deep2/releases/download/parameters/BetterRnnlm.pkl
+
+  cd "${SCRIPT_DIR}"
+}
+
+function download_all() {
+  download_ch06
+}
+
+# -------------------------------------------------------------------------------------------
+
+target=${TARGETS[0]}
+
+# コマンドライン引数の解析
+if [ $# -gt 1 ]; then
+  echo "[Error] number of args is less than 1."
+  exit 1
+elif [ $# -eq 1 ]; then
+  if $(echo ${TARGETS[@]} | grep -q $1); then
+    target=$1
+  else
+    echo "[Error] '$1' is not supported as arg"
+    exit 1
+  fi
+fi
+
+# アクションの実行
+if [ ${target} == "all" ]; then
+  echo "download target: '${target}'"
+  download_all
+elif [ ${target} == "ch06" ]; then
+  echo "download target: '${target}'"
+  download_ch06
+else
+
+  echo "ターゲット: '${target}' は存在しません"
+  echo "target: '${target}' is not supported."
+  cd ${SCRIPT_EXECUTED_DIR}
+  exit 1
+fi
+
+cd ${SCRIPT_EXECUTED_DIR}
